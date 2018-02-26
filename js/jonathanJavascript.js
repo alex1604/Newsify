883cd63fa35986d49146da015a16ff95068dc066
JohanssonJonathan <jjohanssonjonathan@gmail.com>
2018-02-26 10:40:43 +0100

    Fixed the tags to be able to search

--- js/jonathanJavascript.js
+++ js/jonathanJavascript.js
@@ -1,7 +1,14 @@
 
 
 
-let currentTag = document.getElementById("currentTag")
+let currentTag = {
+
+  inputTag : document.getElementById("inputTag"),
+  sourceTag : document.getElementById("sourceTag"),
+  countryTag: document.getElementById("countryTag"),
+  categoryTag : document.getElementById("categoryTag"),
+  languageTag: document.getElementById("languageTag")
+}
 
 let previousTagBtn = document.getElementById("prev");
 let nextTagBtn = document.getElementById("next");
@@ -14,7 +21,8 @@ let slider = {
     prevTagBtn: document.getElementById("prevInput"),
     sliderContent : document.getElementById("sliderContent"),
     sliderContentChange: document.getElementById("sliderContentChange"),
-    sourceName : document.getElementById("sourceName")
+    sourceName : document.getElementById("sourceName"),
+    inputKeyword: document.getElementById("inputKeyword")
 }
 
 let sourceSlider = {
@@ -84,6 +92,9 @@ let languageTotalLeft = "";
 
 
 
+
+
+
 // När du klickar på nextInput
 
 slider.nextTagBtn.addEventListener("click",function(){
@@ -99,8 +110,14 @@ slider.nextTagBtn.addEventListener("click",function(){
 
       totalLeft = "-" + totalLeft + "px";
 
+      if(minusSlide === 1){
 
-
+          if(slider.inputKeyword.value === ""){
+          currentTag.inputTag.innerHTML = ""
+        }else{
+            currentTag.inputTag.innerHTML = "#"+slider.inputKeyword.value;
+        }
+      }
 
       if(sliderContentChangeLength > minusSlide){
 
@@ -150,7 +167,14 @@ slider.prevTagBtn.addEventListener("click",function(){
   totalLeft = totalLeft.toString();
   totalLeft = "-" + totalLeft + "px";
 
+  if(minusSlide === 1){
 
+    if(slider.inputKeyword.value === ""){
+    currentTag.inputTag.innerHTML = ""
+  }else{
+      currentTag.inputTag.innerHTML = "#"+slider.inputKeyword.value;
+  }
+  }
 
   if(minusSlide>1){
 
@@ -190,18 +214,225 @@ slider.prevTagBtn.addEventListener("click",function(){
 
 })
 
-// function slider(sliderContentChangeLength, totalLeft, minusSlide, sliderContentChangeWidth,slider, sliderContentChange){
-//
-//
-//
-//
-// }
+
+function sourceSwitch(sourceMinusSlide){
+
+  switch (sourceMinusSlide) {
+    case 1:
+        currentTag.sourceTag.innerHTML = " "
+      break;
+    case 2:
+      currentTag.sourceTag.innerHTML = "#CNN"
+      break;
+      case 3:
+        currentTag.sourceTag.innerHTML = "#BBC"
+        break;
+        case 4:
+          currentTag.sourceTag.innerHTML = "#ABCNews"
+          break;
+          case 5:
+            currentTag.sourceTag.innerHTML = "#AlJazeera"
+            break;
+            case 6:
+              currentTag.sourceTag.innerHTML = "#ESPN"
+              break;
+              case 7:
+                currentTag.sourceTag.innerHTML = "#FinancialTimes"
+                break;
+                case 8:
+                  currentTag.sourceTag.innerHTML = "#FoxNews"
+                  break;
+                  case 9:
+                    currentTag.sourceTag.innerHTML = "#DailyMail"
+                    break;
+                    case 10:
+                      currentTag.sourceTag.innerHTML = "#TheGuardian"
+                      break;
+                      case 11:
+                        currentTag.sourceTag.innerHTML = "#TheNewYorkTimes"
+                        break;
+                        case 12:
+                          currentTag.sourceTag.innerHTML = "#IGN"
+                          break;
+                          case 13:
+                            currentTag.sourceTag.innerHTML = "#CNNSpanish"
+                            break;
+                            case 14:
+                              currentTag.sourceTag.innerHTML = "#Bloomberg"
+                              break;
+                              case 15:
+                                currentTag.sourceTag.innerHTML = "#CBS"
+                                break;
+                default:
+
+  }
+}
+
+
+function countrySwitch(countryMinusSlide){
+
+  switch (countryMinusSlide) {
+    case 1:
+      currentTag.countryTag.innerHTML = " ";
+      break;
+    case 2:
+      currentTag.countryTag.innerHTML = "#USA";
+      break;
+    case 3:
+      currentTag.countryTag.innerHTML = "#Canada";
+      break;
+    case 4:
+      currentTag.countryTag.innerHTML = "#England";
+      break;
+    case 5:
+    currentTag.countryTag.innerHTML = "#Germany";
+      break;
+    case 6:
+    currentTag.countryTag.innerHTML = "#France";
+      break;
+    case 7:
+    currentTag.countryTag.innerHTML = "#Australia";
+      break;
+    case 8:
+    currentTag.countryTag.innerHTML = "#NewZeeland";
+      break;
+    case 9:
+    currentTag.countryTag.innerHTML = "#China";
+      break;
+    case 10:
+    currentTag.countryTag.innerHTML = "#Japan";
+      break;
+    case 11:
+    currentTag.countryTag.innerHTML = "#Russia";
+      break;
+    case 12:
+    currentTag.countryTag.innerHTML = "#Spain";
+      break;
+    case 13:
+    currentTag.countryTag.innerHTML = "#Sweden";
+      break;
+    case 14:
+    currentTag.countryTag.innerHTML = "#Norway";
+      break;
+    case 15:
+    currentTag.countryTag.innerHTML = "#Finland";
+      break;
+    case 16:
+    currentTag.countryTag.innerHTML = "#Denmark";
+      break;
+    case 17:
+    currentTag.countryTag.innerHTML = "#Iceland";
+      break;
+    case 18:
+    currentTag.countryTag.innerHTML = "#Turkey";
+      break;
+
+
+    default:
+
+  }
+
+}
+
+
+function categorySwitch(categoryMinusSlide){
+
+  switch (categoryMinusSlide) {
+    case 1:
+      currentTag.categoryTag.innerHTML = " ";
+      break;
+    case 2:
+      currentTag.categoryTag.innerHTML = "#Business";
+      break;
+    case 3:
+      currentTag.categoryTag.innerHTML = "#Entertainment";
+      break;
+    case 4:
+      currentTag.categoryTag.innerHTML = "#General";
+      break;
+    case 5:
+    currentTag.categoryTag.innerHTML = "#Health";
+      break;
+    case 6:
+    currentTag.categoryTag.innerHTML = "#Science";
+      break;
+    case 7:
+    currentTag.categoryTag.innerHTML = "#Sport";
+      break;
+    case 8:
+    currentTag.categoryTag.innerHTML = "#Technology";
+      break;
+
+
+
+    default:
+
+  }
+
+}
+
+
+function languageSwitch(languageMinusSlide){
+
+  switch (languageMinusSlide) {
+    case 1:
+      currentTag.languageTag.innerHTML = " ";
+      break;
+    case 2:
+      currentTag.languageTag.innerHTML = "#Arabic";
+      break;
+    case 3:
+      currentTag.languageTag.innerHTML = "#Danish";
+      break;
+    case 4:
+      currentTag.languageTag.innerHTML = "#English";
+      break;
+    case 5:
+    currentTag.languageTag.innerHTML = "#Spanish";
+      break;
+    case 6:
+    currentTag.languageTag.innerHTML = "#French";
+      break;
+    case 7:
+    currentTag.languageTag.innerHTML = "#Hebrew";
+      break;
+    case 8:
+    currentTag.languageTag.innerHTML = "#Italian";
+      break;
+    case 9:
+    currentTag.languageTag.innerHTML = "#Dutch";
+      break;
+    case 10:
+    currentTag.languageTag.innerHTML = "#Norweigan";
+      break;
+    case 11:
+    currentTag.languageTag.innerHTML = "#Portugues";
+      break;
+    case 12:
+    currentTag.languageTag.innerHTML = "#Russian";
+      break;
+    case 13:
+    currentTag.languageTag.innerHTML = "#Swedish";
+      break;
+    case 14:
+    currentTag.languageTag.innerHTML = "#Chinese";
+      break;
+
+
+
+    default:
+
+  }
+
+}
+
+
 
 sourceSlider.nextBtn.addEventListener("click", function(){
 
 
-  let sourceContentChangeLength = sourceSlider.sourceContentChange.children.length;
 
+  let sourceContentChangeLength = sourceSlider.sourceContentChange.children.length;
   sourceTotalLeft = sourceMinusSlide * sourceContentChangeWidth;
   sourceTotalLeft = sourceTotalLeft.toString();
 
@@ -216,6 +447,10 @@ sourceSlider.nextBtn.addEventListener("click", function(){
 
 
     sourceMinusSlide++
+
+    console.log(sourceMinusSlide)
+
+    sourceSwitch(sourceMinusSlide)
 
 
   }
@@ -247,6 +482,10 @@ sourceSlider.prevBtn.addEventListener("click", function(){
     sourceMinusSlide--
 
 
+    sourceSwitch(sourceMinusSlide)
+
+
+
 
   }
 
@@ -272,6 +511,8 @@ countrySlider.countryNextBtn.addEventListener("click",function(){
 
 
     countryMinusSlide++
+
+    countrySwitch(countryMinusSlide)
   }
 
 
@@ -297,6 +538,7 @@ countrySlider.countryPrevBtn.addEventListener("click",function(){
 
 
     countryMinusSlide--
+    countrySwitch(countryMinusSlide)
 
 
 
@@ -326,6 +568,8 @@ categorySlider.categoryNextBtn.addEventListener("click",function(){
 
     categoryMinusSlide++
 
+    categorySwitch(categoryMinusSlide)
+
   }
 
 
@@ -352,6 +596,7 @@ categorySlider.categoryPrevBtn.addEventListener("click",function(){
 
     categoryMinusSlide--
 
+    categorySwitch(categoryMinusSlide)
 
 
   }
@@ -379,6 +624,8 @@ languageSlider.languangeNextBtn.addEventListener("click",function(){
 
       languageMinusSlide++
 
+      languageSwitch(languageMinusSlide)
+
     }
 
 
@@ -404,6 +651,7 @@ languageSlider.languangePrevBtn.addEventListener("click",function(){
 
 
     languageMinusSlide--
+    languageSwitch(languageMinusSlide)
 
 
 
