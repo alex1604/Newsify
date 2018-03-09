8af384d9ffa0a124aca942c1e4b244c7f2880bd3
Erik8901 <32448249+Erik8901@users.noreply.github.com>
2018-03-09 12:05:41 +0100

    historyfixed

--- js/erikJavascript.js
+++ js/erikJavascript.js
@@ -6,22 +6,34 @@ console.log("erikJS");
        let datum = new Date();
        let date = datum.getDate();
        let month = datum.getMonth() + 1;
-       let link = "http://numbersapi.com/"+month+"/"+date+"/date?json";
+       let link = "http://cors-anywhere.herokuapp.com/http://history.muffinlabs.com/date";
+//       let cors = "http://cors-anywhere.herokuapp.com/"
+      /* let link = "http://numbersapi.com/"+month+"/"+date+"/date?json"; */
        let footer = document.getElementById("footer");
        let divHistory = document.getElementById("divHistory");
-       let div1 = document.getElementById("div1"); 
-       let div2 = document.getElementById("div2"); 
+       let eventYear = document.getElementById("eventYear"); 
+       let eventText = document.getElementById("eventText"); 
        
               
            fetch(link)
            .then(function(response) {
-               console.log(response);
                return response.json();
             })
            .then(function(obj) {
-               console.log(obj.year);
-               console.log(obj.text);
-               let year = JSON.stringify(obj.year);
+               let date = JSON.stringify(obj.date);
+               divHistory.innerHTML = "Today in History: " + date;
+               
+               
+               //console.log(obj.data.Events[42]);
+            let event1Year = JSON.stringify(obj.data.Events[42].year);
+            let event1Text = JSON.stringify(obj.data.Events[42].text);
+                console.log(event1Text);
+            
+               eventYear.innerHTML = "Year: " + event1Year;
+               eventText.innerHTML = event1Text;
+               
+               /*     
+               
                let divYear = document.createElement("div");
                let txt = JSON.stringify(obj.text);
                let divTxt = document.createElement("div");
@@ -31,7 +43,7 @@ console.log("erikJS");
                divTxt.id = "divTxt";
                div1.appendChild(divYear);
                div2.appendChild(divTxt);
-     
+     */
      }) 
         
         var whenScroll = function(evt) {
