a1f08913e810156847752a5d7209eff54aaef99a
Alejandro <92.alejandrogarcia@gmail.com>
2018-03-21 10:46:00 +0100

    Update alejandroJavascript.js

--- js/alejandroJavascript.js
+++ js/alejandroJavascript.js
@@ -175,7 +175,7 @@ var callback = function () {
     getWeather(ltd, lng, urlWeather);
   }
 
-  getLocation();
+  //getLocation();
   //raden är kommenterat pga begränsat antal anrop till API:t
 
 
